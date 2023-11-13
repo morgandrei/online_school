5bab25cd01d1200f5395b61ebc0a7c10a1ce56b6
Andrey <moregun@list.ru>
2023-11-13 22:58:38 +0300

    5

--- payment/views.py
+++ payment/views.py
@@ -1,20 +1,13 @@
-from django.shortcuts import render
-from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
+
+from payment.filters import PaymentFilter
 from payment.models import Payment
 from payment.serializers import PaymentSerializer
 
 
-class PaymentCreateAPIView(generics.CreateAPIView):
+class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
-
-
-class PaymentListAPIView(generics.ListAPIView):
-    serializer_class = PaymentSerializer
-    queryset = Payment.objects.all()
-
-    filter_backends = [OrderingFilter, DjangoFilterBackend]
-    ordering_fields = ('pay_date',)
-    filterset_fields = ('pay_course', 'pay_lesson', 'pay_method')
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = PaymentFilter
