e3e65e3ce406a836a26eef4091469b843e37a7c1
morgandrei <moregun@list.ru>
2024-01-29 16:39:52 +0300

    update task's

--- school/task.py
+++ school/task.py
@@ -8,12 +8,11 @@ from school.models import Subscription, Course
 def send_subscription_notification(course_id):
     course = Course.objects.get(id=course_id)
     subscribers = Subscription.objects.filter(course=course, subscribed=True)
-
-    for subscriber in subscribers:
-        send_mail(
+    recipient_list = [s.user.email for s in subscribers]
+    send_mail(
             subject='Уведомление об обновлении',
             message=f'В курс {course.title} был добавлен урок.',
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[subscriber.user.email],
+            recipient_list=recipient_list,
             fail_silently=False,
         )
--- users/task.py
+++ users/task.py
@@ -6,8 +6,4 @@ from django.utils import timezone
 @shared_task
 def blocking_inactive_users():
     threshold = timezone.now() - timezone.timedelta(days=30)
-    inactive = User.objects.filter(last_login__lt=threshold, is_active=True)
-
-    for user in inactive:
-        user.is_active = False
-        user.save()
+    User.objects.filter(last_login__lt=threshold, is_active=True).update(is_active=False)
